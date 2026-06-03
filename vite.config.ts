e02d426d4f18ba8beb5a2b0a3a4cfc01f6cca348
Allgaming796 <namanjha902@gmail.com>
2026-06-03 12:54:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/TutorFind-India/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
